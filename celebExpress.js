d75969036f3552c4c382fb713ddc2654815f0f54
DavideDaniel <davidedaniel@gmail.com>
2015-01-22 13:17:51 -0500

    completed

--- celebExpress.js
+++ celebExpress.js
@@ -2,14 +2,14 @@ var express = require( "express" )
 var ejs = require( "ejs" )
 var app = express();
 
-var celebName = "BEAN";
+var celebName = "CHUCK PALAHNIUK";
 var celeb = {
 	results: {
-		"profession": "actor",
-		"known_for": [ "movie star", "cloning", "comedy" ],
-		"gender": "",
-		"first letter of first name": "m",
-		"extra hint": "Time out!"
+		"profession": "Writer",
+		"known_for": [ "writing awesome books and being awesome", "Portlander", "funny poignant books", "fiction" ],
+		"gender": "male",
+		"first letter of first name": "C",
+		"quotes": ["Find joy in everything you choose to do. Every job, relationship, home... it's your responsibility to love it, or change it.", "Our Generation has had no Great war, no Great Depression. Our war is spiritual. Our depression is our lives.", "We all die. The goal isn't to live forever, the goal is to create something that will."]
 	}
 }
 
--- celebHTTP.rb
+++ celebHTTP.rb
@@ -19,7 +19,7 @@ while (!correct) do
     puts "Make a guess"
     guess = gets.chomp
     guess_url = url + guess
-    guess_url = guess_url.gsub!(" ", "%20")
+    guess_url = guess_url.gsub(" ", "%20")
     puts guess_url
     answer = HTTParty.get(guess_url)
     if answer.keys[0] == "correct"
