d91bd5795eecae7427b088d2794d2f946c59be87
DavideDaniel <davidedaniel@gmail.com>
2015-01-22 14:17:23 -0500

    added hint

--- celebExpress.js
+++ celebExpress.js
@@ -10,7 +10,9 @@ var celeb = {
 		"known_for": [ "writing awesome books and being awesome", "Portlander", "funny poignant books", "fiction" ],
 		"gender": "male",
 		"first letter of first name": "C",
-		"quotes": ["Find joy in everything you choose to do. Every job, relationship, home... it's your responsibility to love it, or change it.", "Our Generation has had no Great war, no Great Depression. Our war is spiritual. Our depression is our lives.", "We all die. The goal isn't to live forever, the goal is to create something that will."]
+		"quotes": [ "Our Generation has had no Great war, no Great Depression. Our war is spiritual. Our depression is our lives.", "We all die. The goal isn't to live forever, the goal is to create something that will.", "Find joy in everything you choose to do. Every job, relationship, home... it's your responsibility to love it, or change it." ],
+		"another hint": "The first rule is... we don't talk about it..."
+
 	}
 }
 
